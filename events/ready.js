3c43a1ebe0e06ba3fb78f55390691edee106b7c7
CodeCannibals <itsmeacktaryt@gmail.com>
2020-08-23 12:50:49 +0400

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,10 @@
+module.exports = (client) => {
+  client.user.setActivity(client.config.status);
+  console.log("=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log("Name: CloudServer Alpha");
+  console.log("Version: 1.0.0");
+  console.log("Loaded: true");
+  console.log("Developer: AcktarOfficial#6930");
+  console.log("Website: acktarofficial.com");
+  console.log("=+=+=+=+=+=+=+=+=+=+=+=");
+} 
